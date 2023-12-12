cc568e09b89128de73a5e2e861024fa1121a4c78
Princess-Morris <okpataprincessmorris3@gmail.com>
2023-12-12 11:08:21 +0100

    added a function to delete feedback

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
-import FeedbackItem from "./components/FeedbackItem";
 import FeedbackList from "./components/FeedbackList";
 import Card from "./components/shared/Card";
 
-function App() {
+function App() { 
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const deleteFeedbck = (id) => {
+    if(window.confirm('Are you sure you want to delete?')){
+      setFeedback(feedback.filter((item) => (item.id !== id)))
+    }
+  }
 
   return (
     <>
     <Header />
    <div className="container">
-    <FeedbackList feedback={feedback} />
-    <Card>Helo </Card>
+    <FeedbackList feedback={feedback}
+    handleDelete={deleteFeedbck}
+     />
    </div>
    </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,15 +1,28 @@
+import {FaTimes} from 'react-icons/fa'
+import PropTypes from 'prop-types';
 import Card from "./shared/Card";
 
-function FeedbackItem ({item}) {
+function FeedbackItem ({item, handleDelete}) {
+
+  const handleClick = (id) => {
+    console.log(`the id is:${id}`)
+  }
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
       <div className="text-display">
         {item.text}
       </div>
     </Card>
   );
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem;
