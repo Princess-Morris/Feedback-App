5ae79c49173962756ae5c63c4b3ddbf534965ff9
Princess-Morris <okpataprincessmorris3@gmail.com>
2023-12-18 09:46:32 +0100

    created a state and a function to edit each feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,27 +7,54 @@ export const FeedbackProvider = ({children}) => {
     const [feedback, setFeedback] = useState([
         {
             id: 1,
-            text: 'This item is from context',
+            text: 'This is feedback item 1',
             rating: 10
+        },
+        {
+            id: 2,
+            text: "This is feedback item 2",
+            rating: 8
+        },
+        {
+            id: 3,
+            text: "This is feedback item 3",
+            rating: 6
         }
     ])
 
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
+
+    // delete feedback
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')){
           setFeedback(feedback.filter((item) => (item.id !== id)))
         }
       }
 
+      // Add feedback
       const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
          setFeedback([newFeedback, ...feedback])
+      }
+
+      // Set item to be updated
+      const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
       }
 
     return <FeedbackContext.Provider 
     value={{
        feedback,
        deleteFeedback,
-       addFeedback
+       addFeedback,
+       editFeedback,
+       feedbackEdit
     }} 
     >
          {children}
